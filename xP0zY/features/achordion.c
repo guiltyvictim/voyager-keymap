e07510831769b16c2e0c517c3b4233bdd86a84ab
Alex <guiltyvictim@gmail.com>
2024-11-29 11:00:16 +0000

    Tweaked layer change timing

--- xP0zY/features/achordion.c
+++ xP0zY/features/achordion.c
@@ -340,7 +340,14 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
 
 // By default, the timeout is 1000 ms for all keys.
 __attribute__((weak)) uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
-  return 1000;
+  switch (tap_hold_keycode) {
+    case KC_ENTER:
+    case KC_TAB:
+    case KC_SPACE:
+    case KC_BSPC:
+      return 0;  // Bypass Achordion for these keys.
+  }
+  return 500;
 }
 
 // By default, Shift and Ctrl mods are eager, and Alt and GUI are not.
