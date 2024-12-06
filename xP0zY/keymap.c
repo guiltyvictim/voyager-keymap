27f402c76a9c61d2147fbf6f78a8b84f64df010a
Alex <guiltyvictim@gmail.com>
2024-12-06 18:42:47 +0000

    Tweaking override code

--- xP0zY/keymap.c
+++ xP0zY/keymap.c
@@ -278,8 +278,8 @@ void matrix_scan_user(void) {
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 // This globally defines all key overrides to be used
-const key_override_t *key_overrides[] = {
+const key_override_t **key_overrides = (const key_override_t *[]){
 	&delete_key_override,
-  NULL
+	NULL // Null terminate the array of overrides!
 };
 
